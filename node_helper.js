76138768096e4628c207f08d6d44e99394bd24fb
Harm Tilburgs <24824989+htilburgs@users.noreply.github.com>
2019-04-10 15:05:47 +0200

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -22,9 +22,8 @@ module.exports = NodeHelper.create({
 	}, 
 	(error, response, body) => {
 		if (!error && response.statusCode == 200) {
-//		var result = JSON.parse(body).roadEntries;				// JSON data path - object.roadEntries.x 
-	var result = JSON.parse(body).roadEntries.forEach(function(element) {console.log(element.result);
-//		console.log(response.statusCode + result);				// uncomment to see in terminal
+		var result = JSON.parse(body).roadEntries;				// JSON data path - object.roadEntries.x 
+		console.log(response.statusCode + result);				// uncomment to see in terminal
 		this.sendSocketNotification('MYTRAFFIC_RESULT', result);
 		}
         });
