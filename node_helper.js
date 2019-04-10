94e7897271b2728b69f5527ea39435011cf87eae
Harm Tilburgs <24824989+htilburgs@users.noreply.github.com>
2019-04-10 19:31:18 +0200

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -23,27 +23,6 @@ module.exports = NodeHelper.create({
 	(error, response, body) => {
 		if (!error && response.statusCode == 200) {
 		var result = JSON.parse(body).roadEntries;				// JSON data path - object.roadEntries.x 
-					
-		// Create lists of jams, construction-zones and radar positions, with their road name	
-		var jams = []
-		var construction_zones= []
-		var radars = []
-
-		for(var road of result){
-  			for (var jam of road.events.trafficjams){
-			jams.push({name: road.road, jam})
-			}
-  			for (var construction of road.events.roadWorks){
-     			construction_zones.push({name: road.road,construction})
-  			}
-  			for (var radar of road.events.radars){
-     			radars.push({name: road.road,radar})
-  			}
-		}
-			
-		console.log(response.statusCode + result);	// uncomment to see in terminal
-		this.sendSocketNotification('MYTRAFFIC_RESULT', result);
-		}
         });
     },
 
