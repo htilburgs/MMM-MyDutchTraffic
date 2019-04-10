8d06880fd3031ebd1e9f99a64646ab2cf5140bc4
Harm Tilburgs <24824989+htilburgs@users.noreply.github.com>
2019-04-10 22:43:41 +0200

    Update MMM-MyTraffic.js

--- MMM-MyTraffic.js
+++ MMM-MyTraffic.js
@@ -96,7 +96,7 @@ Module.register('MMM-MyTraffic', {
 		for (var i = 0; i < jams.length; i++) {		
 			var jamsNameRow = document.createElement("tr");
 			jamsNameRow.className = "small";
-			jamsNameRow.innerHTML = '<i class="fas fa-car"></i>' + jams[i].name;
+			jamsNameRow.innerHTML = '<i class="fas fa-car"></i> ' + jams[i].name;
 			table.appendChild(jamsNameRow);
 			
 			var jamsReasonRow = document.createElement("tr");
@@ -117,7 +117,7 @@ Module.register('MMM-MyTraffic', {
 		for (var i = 0; i < radars.length; i++) {			
 			var radarsRow = document.createElement("tr");
 			radarsRow.className = "small";
-			radarsRow.innerHTML = '<i class="fas fa-camera"></i>' + radars[i].name + " - " + radars[i].radar.description;
+			radarsRow.innerHTML = '<i class="fas fa-camera"></i> ' + radars[i].name + " - " + radars[i].radar.description;
 			table.appendChild(radarsRow);
 		}
 		
