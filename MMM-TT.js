d72778e20d042c898f0a3f5a8e1abd6bfc61a5f1
Harm Tilburgs <24824989+htilburgs@users.noreply.github.com>
2019-04-16 23:08:20 +0200

    Update MMM-TT.js

--- MMM-TT.js
+++ MMM-TT.js
@@ -10,22 +10,24 @@ Module.register('MMM-TT', {
 
 	// Default values
 	defaults: {
-		showJams: true,						// Show Traffic jams
-		showConstructions: true,			// Show Constructions
-		showRadars: true,					// Show Radar controles
-		preferredRoads: ['ALL'],				// Display only preferred roads - All is everything, other "A1",A2",..
-		maxWidth: "500px",					// Max width wrapper
-		animationSpeed: 1000, 				// fade in and out speed
+		showJams: true,				// Show Traffic jams
+		showConstructions: true,		// Show Constructions
+		showRadars: true,			// Show Radar controles
+		preferredRoads: ['ALL'],		// Display only preferred roads - All is everything, other "A1",A2",..
+		maxWidth: "500px",			// Max width wrapper
+		animationSpeed: 1000, 			// fade in and out speed
 		initialLoadDelay: 1000,
 		retryDelay: 2500,
 		updateInterval: 10 * 60 * 1000		// every 10 minutes
 	},
 
-	MTR: null,			
+			
 	// Create lists of jams, construction-zones and radar positions, with their road name	
+	MTR: null,	
 	jams : [],
 	constructions : [],
-	radars : [],	
+	radars : [],
+	
 	// Define stylesheet
 	getStyles: function () {
 		return ["MMM-TT.css"];
@@ -45,7 +47,7 @@ Module.register('MMM-TT', {
 	},
 	
 	start: function () {
-    mself=this;
+   		mself=this;
 		Log.info("Starting module: " + this.name);
 		requiresVersion: "2.1.0",	
 			
@@ -63,24 +65,12 @@ Module.register('MMM-TT', {
 		wrapper.style.maxWidth = this.config.maxWidth;
 	
 		// The loading sequence
-    if (!this.loaded) {
-            wrapper.innerHTML = "Loading....";
-            wrapper.classList.add("bright", "light", "small");
-            return wrapper;
-    }	
-
-
-/*		for (var road of MTR.roadEntries){
-  			for (var jam of road.events.trafficJams){
-			jams.push({name: road.road, jam})			
-			}
-  			for (var construction of road.events.roadWorks){
-    			constructions.push({name: road.road,construction})
-  			}
-  			for (var radar of road.events.radars){
-    			radars.push({name: road.road,radar})
-  			}
-		} */ 
+   		if (!this.loaded) {
+            	wrapper.innerHTML = "Loading....";
+           	wrapper.classList.add("bright", "light", "small");
+            	
+		return wrapper;
+	}	
 	
 		//Display Traffic Jam information
 		if (this.config.showJams != false) {
@@ -113,51 +103,53 @@ Module.register('MMM-TT', {
 			   
 		//Display Traffic Camera (Radar) information
 		if (this.config.showRadars != false) {		
-		  for (var r of this.radars) {	
-        var warnWrapper = document.createElement("div");
-        var icon = document.createElement("div");
-        icon.classList.add('trafficicon-camera', 'small-icon');
-        var event = document.createElement("div");
-        event.className = "event xsmall";
-        var information = document.createElement("div");
-        information.className = "bold"
-        information.innerHTML = r.radar.location;
-        var description = document.createElement("div");
-        description.className.add = "description xsmall";
-        description.innerHTML = r.radar.description;
-        var horLine = document.createElement("hr");
-        event.appendChild(information);
-        event.appendChild(description);
-        warnWrapper.appendChild(icon);
-        warnWrapper.appendChild(event);
-        wrapper.appendChild(warnWrapper);
-        wrapper.appendChild(horLine); 
-		   }
+		for (var r of this.radars) {
+			
+        		var warnWrapper = document.createElement("div");
+        		var icon = document.createElement("div");
+        		icon.classList.add('trafficicon-camera', 'small-icon');
+        		var event = document.createElement("div");
+        		event.className = "event xsmall";
+        		var information = document.createElement("div");
+        		information.className = "bold"
+        		information.innerHTML = r.radar.location;
+        		var description = document.createElement("div");
+        		description.className.add = "description xsmall";
+        		description.innerHTML = r.radar.description;
+        		var horLine = document.createElement("hr");
+        		event.appendChild(information);
+        		event.appendChild(description);
+        		warnWrapper.appendChild(icon);
+        		warnWrapper.appendChild(event);
+        		wrapper.appendChild(warnWrapper);
+        		wrapper.appendChild(horLine); 
+			}
 		}
 				
 		//Display Traffic Constructions information
 		if (this.config.showConstructions != false) {		
-		  for (var c of this.constructions) {	
-        var warnWrapper = document.createElement("div");
-        var icon = document.createElement("div");
-        icon.classList.add('trafficicon-construction', 'small-icon');
-        var event = document.createElement("div");
-        event.className = "event xsmall";
-        var information = document.createElement("div");
-        information.className = "bold"
-        information.innerHTML = c.name + " - " + c.construction.startDate + " t/m " + c.construction.stopDate;
-        var description = document.createElement("div");
-        description.className.add = "description xsmall";
-        description.innerHTML = c.construction.description;
-        var horLine = document.createElement("hr");
-        event.appendChild(information);
-        event.appendChild(description);
-        warnWrapper.appendChild(icon);
-        warnWrapper.appendChild(event);
-        wrapper.appendChild(warnWrapper);
-        wrapper.appendChild(horLine);
-		   }
-		}	
+		for (var c of this.constructions) {	
+        		var warnWrapper = document.createElement("div");
+        		var icon = document.createElement("div");
+        		icon.classList.add('trafficicon-construction', 'small-icon');
+        		var event = document.createElement("div");
+        		event.className = "event xsmall";
+        		var information = document.createElement("div");
+        		information.className = "bold"
+        		information.innerHTML = c.name + " - " + c.construction.startDate + " t/m " + c.construction.stopDate;
+        		var description = document.createElement("div");
+        		description.className.add = "description xsmall";
+        		description.innerHTML = c.construction.description;
+        		var horLine = document.createElement("hr");
+        		event.appendChild(information);
+        		event.appendChild(description);
+        		warnWrapper.appendChild(icon);
+        		warnWrapper.appendChild(event);
+        		wrapper.appendChild(warnWrapper);
+        		wrapper.appendChild(horLine);
+			}
+		}
+		
 		return wrapper;
 	}, // <-- closes the getDom function from above
 		
@@ -165,34 +157,38 @@ Module.register('MMM-TT', {
 	// this processes your data
 	processTRAFFIC: function (data) { 
 		mself.MTR = data; 
-    mself.jams=[]
-    mself.constructions=[]
-    mself.radars=[]
-    for (var road of mself.MTR.roadEntries){
-      Log.log(" typeof="+typeof mself.config.preferredRoads )
-      if(mself.config.preferredRoads.includes(road.road) || mself.config.preferredRoads.includes("ALL")) 
-      { 
-        for (var j1 of road.events.trafficJams){  
-            Log.log("pushing entry for road="+ road.road)        
-            mself.jams.push({name: road.road, jam:j1})
-          }
-        for (var construction of road.events.roadWorks){
-          mself.constructions.push({name: road.road,construction:construction})
-        }
-        for (var radar of road.events.radars){
-          mself.radars.push({name: road.road,radar:radar})
-        }
-      }
+    		mself.jams=[]
+    		mself.constructions=[]
+    		mself.radars=[]
+    		
+		for (var road of mself.MTR.roadEntries){
+      			Log.log(" typeof="+typeof mself.config.preferredRoads )
+      			if(mself.config.preferredRoads.includes(road.road) || mself.config.preferredRoads.includes("ALL")) 
+      			{
+			
+        		for (var j1 of road.events.trafficJams){  
+            		Log.log("pushing entry for road="+ road.road)        
+            		mself.jams.push({name: road.road, jam:j1})
+          		}
+			
+        		for (var construction of road.events.roadWorks){
+          		mself.constructions.push({name: road.road,construction:construction})
+        		}
+			
+        		for (var radar of road.events.radars){
+          		mself.radars.push({name: road.road,radar:radar})
+        		}
 		}
+	}
+		
 //		console.log(mself.MTR); // uncomment to see if you're getting data (in dev console)
 		mself.loaded = true;
-
 	},
 	
 	// this tells module when to update
 	scheduleUpdate: function () { 
 		setInterval(() => {
-		   this.getTRAFFIC();
+		this.getTRAFFIC();
 		}, this.config.updateInterval);
 		this.getTRAFFIC();
 		var self = this;
@@ -206,8 +202,8 @@ Module.register('MMM-TT', {
 	// this gets data from node_helper
 	socketNotificationReceived: function(notification, payload) { 
 		if (notification === "MYTRAFFIC_RESULT") {
-		    this.processTRAFFIC(payload);
-        mself.updateDom(100);
+		this.processTRAFFIC(payload);
+        	mself.updateDom(100);
 		}
 		//this.updateDom(this.config.initialLoadDelay);
 	},
