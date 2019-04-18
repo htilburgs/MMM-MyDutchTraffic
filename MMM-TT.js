7e4ecc8d950953709b17496aa147c3f5f7c48924
Harm Tilburgs <24824989+htilburgs@users.noreply.github.com>
2019-04-18 07:53:17 +0200

    Update MMM-TT.js

--- MMM-TT.js
+++ MMM-TT.js
@@ -168,9 +168,7 @@ Module.register('MMM-TT', {
 				var jamsDescription = document.createElement("div");
 				jamsDescription.className = "jamsDescription xsmall";
 				jamsDescription.innerHTML = j.jam.description;
-				var horLine = document.createElement("hr");
 				wrapper.appendChild(jamsDescription);
-				wrapper.appendChild(horLine);
 		   		}
 			}
 /*				
