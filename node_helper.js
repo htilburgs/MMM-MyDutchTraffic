fd25565ecb1ba6cdc5b49cef9ab728c35bbdab11
Harm Tilburgs <harm@tilburgs.com>
2024-12-30 18:09:14 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -4,44 +4,40 @@ MMM-MyDutchTraffic
 Copyright (C) 2024 - H. Tilburgs
 MIT License
 
-v1.0	: Initial release
-
+v1.0: Initial release
+v2.0: Optimize code
 //-------------------------------------------
 */
 
 const NodeHelper = require('node_helper');
 
 module.exports = NodeHelper.create({
-
-  start: function() {
-          console.log("Starting node_helper for: " + this.name);
+  start() {
+    console.log(`Starting node_helper for: ${this.name}`);
   },
 
-getMDT: function(url) {
-        // Make a GET request using the Fetch API
-        fetch(url)
-          .then(response => {
-            if (!response.ok) {
-              console.error('MMM-MyDutchTraffic: Network response was not ok');
-            }
-            return response.json();
-          })
+  async getMDT(url) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        console.error('MMM-MyDutchTraffic: Network response was not ok', response.status);
+        return;
+      }
 
-          .then(result => {
-            // Process the retrieved user data
-            // console.log(result.data.timings); // Remove trailing slashes to display data in Console for testing
-            this.sendSocketNotification('MDT_RESULT', result);
-          })
-
-          .catch(error => {
-            console.error('Error:', error);
-          });
+      const result = await response.json();
+      if (result) {
+        this.sendSocketNotification('MDT_RESULT', result);
+      } else {
+        console.error('MMM-MyDutchTraffic: Invalid or empty response data');
+      }
+    } catch (error) {
+      console.error('MMM-MyDutchTraffic: Error fetching data', error);
+    }
   },
 
- socketNotificationReceived: function(notification, payload) {
-            if (notification === 'GET_MDT') {
-            this.getMDT(payload);
-            }
+  socketNotificationReceived(notification, payload) {
+    if (notification === 'GET_MDT') {
+      this.getMDT(payload);
+    }
   },
-
 });
